11af02ceedde86f966030cf17b63ae887c29d87e
[Shawn Hsueh] <[shawionghsueh@utexas.edu]>
2021-01-18 16:51:05 -0600

    yoyo light sorta works, before custom display driver

--- Yoyo_Light/Yoyo_Light.ino
+++ Yoyo_Light/Yoyo_Light.ino
@@ -9,6 +9,7 @@
 #include <BlynkSimpleEsp8266.h>
 #include "ESP8266TimerInterrupt.h"
 #include "ESP8266_ISR_Timer.h"
+#include "Display_Map.h"
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
@@ -23,8 +24,6 @@ char pass[32] = "idonotlikewholewheatbread";
 
 const int LED_PIN = 0;
 const int NUM_LEDS = 10;  //radius (# of leds)
-const int NUM_SECTORS = 32; //circumference (resolution in the polar axis)
-const float theta = 360 / NUM_SECTORS;  //angle per sector
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 ESP8266Timer ITimer;
@@ -37,11 +36,11 @@ MPU6050 imu2(0x69); //AD0 high
 const float SENSOR_DISTANCE = 0.0272;
 const float GRAVITY_ACCEL = 9.8;
 
-//0,1,2 --> x,y,z
-float ay1_vect[3];
-float ay2_vect[3];
+float a1_c; //centripetal acceleration
+float a2_c;
 float calib = 0;
 
+//0,1,2 --> x,y,z
 int16_t a1[3];
 int16_t a2[3];
 // volatile for when accessed by interrupts
@@ -49,51 +48,54 @@ volatile float diff = 0;
 volatile float ang_vel = 0;
 volatile float dps = 0;
 float gyro_dps = 0;
-float prevDiff = 0;
 float rpm = 0;
-volatile float correction = 1;
-volatile int threshold = 1;
 
-bool toggle = false;
+float prevDiff = 0; //prev accel difference to determine change in state
+float threshold = 1; //diff threshold (based on ang vel) to start POV sequence
+volatile float correction = 1;
 
 int hw_timer_interval = 1;
 int update_display_interval = 50; //doesn't really matter since it'll get updated again
-int update_sensor_interval = 5; //in ms
+int update_sensor_interval = 1; //in ms
 int send_blynk_interval = 200;
 
-uint8_t displayLed[NUM_SECTORS][NUM_LEDS]; //matrix for displaying led
-uint8_t sectorStage = 0;  //curr sector
+const int NUM_SECTORS = 50; //circumference (resolution in the polar axis)
+const float THETA = 360 / NUM_SECTORS;  //angle per sector
 
-#define abs(x) ((x)>0?(x):-(x))
+volatile uint8_t displayLed[NUM_SECTORS][NUM_LEDS]; //matrix for displaying led
+volatile uint8_t sectorStage = 0;  //curr sector
+volatile uint8_t brightness = 100;
+
+const int NUM_READINGS = 4;
+int readIndex = 0;
+float readings[NUM_READINGS];
+float total = 0;
+volatile float avg_dps = 0;
+
+#define abs(x) ((x)>0?(x):-(x)) //esp8266 doesn't have abs for floats for some reason, so this works for floats
 
 void sendBlynkEvent() {
-  String msg = String(dps) + ":" + String(diff);
+  String msg = String(avg_dps) + ":" + String(diff);
   Blynk.virtualWrite(3, msg);
-  //  Serial.println("Send Blynk Event");
 }
 
-//Updates the ISR Timer. 
+//Updates the ISR Timer.
 //Make sure hw_timer_interval is less than other intervals to have accurate interrupts
 void ICACHE_RAM_ATTR TimerHandler() {
   ISR_Timer.run();
 }
 
 void updateDisplay() {
-  if (sectorStage >= 0 && sectorStage <= 15) {
-    for (int i = 0; i < NUM_LEDS; i++) {
-      pixels.setPixelColor(i, pixels.Color(0, 50, 0));
-    }
-  } else {
-    for (int i = 0; i < NUM_LEDS; i++) {
-      pixels.setPixelColor(i, pixels.Color(0, 0, 0));
-    }
+  for (uint8_t i = 0; i < NUM_LEDS; i++) {
+    uint8_t state = displayLed[sectorStage][NUM_LEDS - i]; //0 or 1
+    pixels.setPixelColor(i, pixels.Color(0, brightness * state, 0));
   }
-  //  toggle = !toggle;
   pixels.show();
 
   sectorStage = (sectorStage + 1) % NUM_SECTORS;
 
-  update_display_interval = theta * 1000 / abs(dps); //time inside the sector in ms
+  // update
+  update_display_interval = THETA * 1000 / abs(avg_dps); //time inside the sector in ms
   ISR_Timer.changeInterval(0, update_display_interval); // update timer 0
   //  Serial.println("Update Display");
 }
@@ -102,7 +104,6 @@ void updateDisplay() {
 //updates the current rpm value
 void readSensorEvent()
 {
-
   //  dps = correction * imu.getRotationZ() / 16.4; //converts sensor reading to degrees/second
   //  rpm = dps * 60 / 360;  //dps to rpm
   imu1.getAcceleration(&a1[0], &a1[1], &a1[2]);
@@ -110,19 +111,27 @@ void readSensorEvent()
 
   // the two sensors are aligned 180 from each other
   // -1 on one imu will normalize the measurements to assume they're facing same direction
-  for (int i = 0; i < 3; i++) {
-    ay1_vect[i] = -a1[i] / 2048.0;
-    ay2_vect[i] = a2[i] / 2048.0; //reads sensor while still and tries to make sure diff is as close to 0
-  }
+  // 2048 normalize to 1g when perpendicular
+  a1_c = -a1[0] / 2048.0;
+  a2_c = a2[0] / 2048.0;
 
-  ay1_vect[0] += 0.01;
-  ay2_vect[0] -= 0.03;
-
-  diff = abs(ay1_vect[0] - ay2_vect[0]);
-  // diff * gravity b/c diff is in g's, normalize into m/s
-  ang_vel = correction * sqrt(diff * GRAVITY_ACCEL / SENSOR_DISTANCE);
+  //adjustments to make it exactly 1g when undisturbed
+  //idk if I should add or multiply to get to 1 ehh who knows
+  a1_c += 0.01;
+  a2_c -= 0.03;
+  // diff * gravity to convert from g to m/s
+  diff = abs(a1_c - a1_c) * GRAVITY_ACCEL;
+  ang_vel = correction * sqrt(diff / SENSOR_DISTANCE);
   rpm = 60 * ang_vel / (2 * 3.141592);
-  dps = rpm * 6; //correction changes the dps value
+  dps = rpm * 6; 
+
+  //create a moving average for dps
+  total = total - readings[readIndex];
+  readings[readIndex] = dps;
+  total += readings[readIndex];
+  readIndex = (readIndex + 1) % NUM_READINGS;
+
+  avg_dps = total / NUM_READINGS;
 
   Serial.print(diff);
   Serial.print("\t");
@@ -136,7 +145,7 @@ void readSensorEvent()
   Serial.print("\t");
   Serial.println(dps);
 
-  //  update_display_interval = theta * 1000000 / abs(dps); // converts time till next update to
+  //  update_display_interval = THETA * 1000000 / abs(dps); // converts time till next update to
 
   if (diff > threshold && prevDiff < threshold) { //on beginning of rotation
     ISR_Timer.enableAll();
@@ -148,7 +157,50 @@ void readSensorEvent()
   //  Serial.println("Update Sensor event");
 }
 
+
+//sets up the display led arrays with the image to show
+void initiateLedMatrix() {
+  for (uint8_t i = 0; i < NUM_SECTORS; i++) {
+    for (uint8_t j = 0; j < NUM_LEDS; j++) {
+      if (i == 0) {
+        for (uint8_t k = 0; k < 5; k++) {
+          displayLed[i + k][j] = H[j][k];
+        }
+      }
+      if (i == 6) {
+        for (uint8_t k = 0; k < 5; k++) {
+          displayLed[i + k][j] = E[j][k];
+        }
+      }
+      if (i == 12) {
+        for (uint8_t k = 0; k < 5; k++) {
+          displayLed[i + k][j] = L[j][k];
+        }
+      }
+      if (i == 18) {
+        for (uint8_t k = 0; k < 5; k++) {
+          displayLed[i + k][j] = L[j][k];
+        }
+      }
+      if (i == 24) {
+        for (uint8_t k = 0; k < 5; k++) {
+          displayLed[i + k][j] = O[j][k];
+        }
+      }
+    }
+  }
+
+  // Instantiate the intial position of the display
+  //  sectorStage = NUM_SECTORS * atan(a1[0] / a1[1]) / (2 * 3.14159);
+  Serial.println(sectorStage);
+}
+
 void setup() {
+  for (uint8_t i = 0; i < NUM_READINGS; i++) {
+    readings[i] = 0;
+  }
+  initiateLedMatrix();
+
   Serial.begin(9600);
   Blynk.begin(auth, ssid, pass);
 
@@ -199,6 +251,7 @@ BLYNK_WRITE(V1)
 BLYNK_WRITE(V2)
 {
   int value = param.asInt();
+  brightness = value;
 }
 
 //
