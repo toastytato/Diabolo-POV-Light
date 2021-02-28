87511f2e3712fbed53de8d7786c138099d22aaf9
[Shawn Hsueh] <[shawionghsueh@utexas.edu]>
2021-02-27 23:56:35 -0600

    display updates hardware

--- YoyoLightOTA/Display_Driver.h
+++ YoyoLightOTA/Display_Driver.h
@@ -1,139 +1,226 @@
 #include <Adafruit_NeoPixel.h>
-#include "Generated_Map.h"
+// #include "Generated_Map.h"
 
-#ifndef NUM_LEDS
-#define NUM_LEDS      10 //radius (# of leds)
-#endif
+// #ifndef num_pixels
+// #define num_pixels 10 //radius (# of leds)
+// #endif
 
-#ifndef NUM_SECTORS
-#define NUM_SECTORS   50 //circumference (resolution in the polar axis)
-#endif
+// #ifndef num_sectors
+// #define num_sectors 50 //circumference (resolution in the polar axis)
+// #endif
 
-#define abs(x) ((x)>0?(x):-(x)) //esp8266 doesn't have abs for floats for some reason, so this works for floats
+#define NUM_LEDS 10
 
-#define LED_PIN       0
-#define THETA         360 / NUM_SECTORS  //angle per sector
+#define abs(x) ((x) > 0 ? (x) : -(x)) //esp8266 doesn't have abs for floats for some reason, so this works for floats
+
+#define LED_PIN 0
+#define THETA 360 / num_sectors //angle per sector
 
 class DisplayDriverPOV
 {
-  private:
+private:
+  Adafruit_NeoPixel Pixels = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-    Adafruit_NeoPixel Pixels = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
+  const uint16_t cnt_per_cycle = num_sectors / 3;
 
-    const uint16_t cnt_per_cycle = NUM_SECTORS / 3;
+  uint8_t *display = nullptr;
+  uint8_t num_sectors = 0;
+  uint8_t num_pixels = 0;
+  uint8_t num_colors = 0;
+  volatile uint32_t count = 0;
+  volatile uint8_t curr_sector = 0;
+  volatile uint8_t brightness = 255;
 
-    volatile uint32_t count = 0;
-    volatile uint8_t curr_sector = 0;
-    volatile uint8_t brightness = 255;
+  uint8_t get_pixel_color(uint8_t sector, uint8_t pixel, uint8_t color);
 
-  public:
-    DisplayDriverPOV();
+public:
+  DisplayDriverPOV();
 
-    //
-    void show_sector(bool flip);
-    void show_line(int brightness);
-    void show_boot_up_sequence();
-    void cycle_display(int cycle_time);
-    bool set_curr_sector(uint8_t value);
-    uint8_t get_curr_sector();
-    void increment_sector();
-    void decrement_sector();
-    void increment_count();
-    void set_brightness(uint8_t value);
-    float time_in_sector_ms(float dps);
+  //
+  void print();
+  void set_display_array(uint8_t *display, uint8_t x, uint8_t y, uint8_t z);
+  void show_sector(bool flip);
+  void show_line(int brightness);
+  void show_boot_up_sequence();
+  void cycle_display(int cycle_time);
+  bool set_curr_sector(uint8_t value);
+  uint8_t get_curr_sector();
+  void increment_sector();
+  void decrement_sector();
+  void increment_count();
+  void set_brightness(uint8_t value);
+  float time_in_sector_ms(float dps);
 };
 
-DisplayDriverPOV::DisplayDriverPOV() {
+/***Private Functions***/
+
+uint8_t DisplayDriverPOV::get_pixel_color(uint8_t sector, uint8_t pixel, uint8_t color)
+{
+  return display[sector * num_pixels * num_colors + pixel * num_colors + color];
+}
+
+/***Constructors***/
+
+DisplayDriverPOV::DisplayDriverPOV()
+{
   Pixels.begin();
 }
 
-void DisplayDriverPOV::show_sector(bool flip) {
-  uint8_t r = (sin(count * 2 * PI / cnt_per_cycle) + 1) / 2 * brightness;
-  uint8_t g = (sin(count * 2 * PI / cnt_per_cycle + (2 * PI / 3)) + 1) / 2 * brightness;
-  uint8_t b = (sin(count * 2 * PI / cnt_per_cycle + (4 * PI / 3)) + 1) / 2 * brightness;
+/***Public Functions***/
+
+void DisplayDriverPOV::print()
+{
+  Serial.print("Display Matrix: ");
+  Serial.print(num_sectors);
+  Serial.print(", ");
+  Serial.print(num_pixels);
+  Serial.print(", ");
+  Serial.print(num_colors);
+  Serial.println("");
+
+  for (uint8_t i = 0; i < num_sectors; i++)
+  {
+    for (uint8_t j = 0; j < num_pixels; j++)
+    {
+      for (uint8_t k = 0; k < num_colors; k++)
+      {
+        Serial.print(get_pixel_color(i, j, k));
+        Serial.print(",");
+      }
+      Serial.print(" : ");
+    }
+    Serial.println("");
+  }
+  Serial.println("");
+}
+
+void DisplayDriverPOV::set_display_array(uint8_t *array, uint8_t x, uint8_t y, uint8_t z)
+{
+  display = array;
+  num_sectors = x;
+  num_pixels = y;
+  num_colors = z;
+}
+
+void DisplayDriverPOV::show_sector(bool flip)
+{
+  // uint8_t r = (sin(count * 2 * PI / cnt_per_cycle) + 1) / 2 * brightness;
+  // uint8_t g = (sin(count * 2 * PI / cnt_per_cycle + (2 * PI / 3)) + 1) / 2 * brightness;
+  // uint8_t b = (sin(count * 2 * PI / cnt_per_cycle + (4 * PI / 3)) + 1) / 2 * brightness;
 
   //      uint8_t r = 0;
   //      uint8_t g = 0;
   //      uint8_t b = brightness;
+  if (display == nullptr)
+  {
+    return;
+  }
 
-  for (uint8_t i = 0; i < NUM_LEDS; i++) {
+  for (uint8_t i = 0; i < num_pixels; i++)
+  {
     uint8_t pixel_idx = i;
-    if (flip) {
-      pixel_idx = NUM_LEDS - i - 1;
+    if (flip)
+    {
+      pixel_idx = num_pixels - i - 1;
     }
     //        uint8_t state = displayLed[curr_sector][pixel_idx]; //0 or 1
-    uint8_t state = GENERATED_DISPLAY[curr_sector][pixel_idx];
-    Pixels.setPixelColor(i, Pixels.Color(r * state, g * state, b * state));
+
+    uint8_t r = get_pixel_color(curr_sector, i, 0);
+    uint8_t g = get_pixel_color(curr_sector, i, 1);
+    uint8_t b = get_pixel_color(curr_sector, i, 2);
+    Pixels.setPixelColor(i, Pixels.Color(r, g, b));
   }
   Pixels.show();
 }
 
-void DisplayDriverPOV::show_line(int brightness) {
-  for (uint8_t i = 0; i < NUM_LEDS; i++) {
+void DisplayDriverPOV::show_line(int brightness)
+{
+  for (uint8_t i = 0; i < num_pixels; i++)
+  {
     Pixels.setPixelColor(i, Pixels.Color(0, brightness, 0));
   }
   Pixels.show();
 }
 
-void DisplayDriverPOV::show_boot_up_sequence() {
-  for (uint8_t i = 0; i < NUM_LEDS; i++) {
+void DisplayDriverPOV::show_boot_up_sequence()
+{
+  for (uint8_t i = 0; i < num_pixels; i++)
+  {
     Pixels.setPixelColor(i, Pixels.Color(0, 50, 0));
     Pixels.show();
     delay(25);
   }
-  for (uint8_t i = 0; i < NUM_LEDS; i++) {
+  for (uint8_t i = 0; i < num_pixels; i++)
+  {
     Pixels.setPixelColor(i, Pixels.Color(0, 0, 0));
     Pixels.show();
     delay(25);
   }
 }
-void DisplayDriverPOV::cycle_display(int cycle_time) {
-  for (uint8_t i = 0; i < NUM_SECTORS; i++) {
-    for (uint8_t j = 0; j < NUM_LEDS; j++) {
-      uint8_t state = GENERATED_DISPLAY[i][j];
-      Pixels.setPixelColor(j, Pixels.Color(100 * state, 0, 0));
+void DisplayDriverPOV::cycle_display(int cycle_time)
+{
+  for (uint8_t i = 0; i < num_sectors; i++)
+  {
+    for (uint8_t j = 0; j < num_pixels; j++)
+    {
+      uint8_t r = get_pixel_color(curr_sector, i, 0);
+      uint8_t g = get_pixel_color(curr_sector, i, 1);
+      uint8_t b = get_pixel_color(curr_sector, i, 2);
+      Pixels.setPixelColor(i, Pixels.Color(r, g, b));
     }
     Pixels.show();
-    delay(cycle_time / NUM_SECTORS);
+    delay(cycle_time / num_sectors);
   }
-  for (uint8_t i = 0; i < NUM_LEDS; i++) {
+  for (uint8_t i = 0; i < num_pixels; i++)
+  {
     Pixels.setPixelColor(i, Pixels.Color(0, 0, 0));
   }
   Pixels.show();
 }
 
-bool DisplayDriverPOV::set_curr_sector(uint8_t value) {
-  if (value > NUM_SECTORS) {
+bool DisplayDriverPOV::set_curr_sector(uint8_t value)
+{
+  if (value > num_sectors)
+  {
     return false;
-  } else {
+  }
+  else
+  {
     curr_sector = value;
     return true;
   }
 }
 
-uint8_t DisplayDriverPOV::get_curr_sector() {
+uint8_t DisplayDriverPOV::get_curr_sector()
+{
   return curr_sector;
 }
 
-void ICACHE_RAM_ATTR DisplayDriverPOV::increment_sector() {
-  curr_sector = (curr_sector + 1) % NUM_SECTORS;
+void ICACHE_RAM_ATTR DisplayDriverPOV::increment_sector()
+{
+  curr_sector = (curr_sector + 1) % num_sectors;
 }
 
-void ICACHE_RAM_ATTR DisplayDriverPOV::decrement_sector() {
-  if (curr_sector == 0) {
-    curr_sector = NUM_SECTORS;
+void ICACHE_RAM_ATTR DisplayDriverPOV::decrement_sector()
+{
+  if (curr_sector == 0)
+  {
+    curr_sector = num_sectors;
   }
   curr_sector--;
 }
 
-void ICACHE_RAM_ATTR DisplayDriverPOV::increment_count() {
+void ICACHE_RAM_ATTR DisplayDriverPOV::increment_count()
+{
   count++;
 }
 
-void DisplayDriverPOV::set_brightness(uint8_t value) {
+void DisplayDriverPOV::set_brightness(uint8_t value)
+{
   brightness = value;
 }
 
-float ICACHE_RAM_ATTR DisplayDriverPOV::time_in_sector_ms(float dps) {
+float ICACHE_RAM_ATTR DisplayDriverPOV::time_in_sector_ms(float dps)
+{
   return THETA * 1000 / abs(dps); //time inside the sector in ms
 }
--- YoyoLightOTA/YoyoLightOTA.ino
+++ YoyoLightOTA/YoyoLightOTA.ino
@@ -7,7 +7,6 @@
 
 // Project Libraries
 #include <MPU6050.h>
-#include <Adafruit_NeoPixel.h>
 #include "ESP8266TimerInterrupt.h"
 #include "ESP8266_ISR_Timer.h"
 #include "Display_Driver.h"
@@ -19,11 +18,23 @@
  *  2/14/21 - Don't just upload copy+pasted code lmao. I ended up bricking the ESP8266 
  *  by uploading the <Wifi.h> library instead of the <ESP8266WiFi.h> so now I can't 
  *  upload new code and yeah... new esp8266 for 13 bucks let's gooooo
- * 
+ *  2/23/21 - So I tried uploading a Blink sketch at 57600 upload speed and it worked somehow? 
+ *  I also tried grounding GPIO 0 to GND several times which didn't work, but perhaps it was 
+ *  from grounding it when I plugged in my USB? ig 
+ *  2/24/21 - I2C Don't work no more :( Can't communicate with IMU 
  */
 
-const char* ssid = SSID;
-const char* password = PASS;
+/*
+ Buffer Map:
+ Update:  [0][sector][x_size][y_size][z_size][...data...]
+ Control: [1][sector][]
+ Change:  [2][wifi_polling_rate_ms][]
+ Ignore:  [# not above]
+ *
+ *
+ *
+ 
+ */
 
 ESP8266Timer ITimer;
 ESP8266_ISR_Timer ISR_Timer;
@@ -32,13 +43,15 @@ DisplayDriverPOV Driver;
 MPU6050 imu1(0x68); //AD0 low
 MPU6050 imu2(0x69); //AD0 high
 
-#define TIMER_0   0
-#define TIMER_1   1
+WiFiUDP udp;
 
-#define SENSOR_DISTANCE   0.0272
-#define GRAVITY_ACCEL     9.8
+#define TIMER_0 0
+#define TIMER_1 1
 
-#define BTN_INTERRUPT_PIN   2
+#define SENSOR_DISTANCE 0.0272
+#define GRAVITY_ACCEL 9.8
+
+#define BTN_INTERRUPT_PIN 2
 
 volatile bool led_state = false;
 
@@ -70,18 +83,27 @@ const float threshold = 2; //diff threshold (based on ang vel m/s) to start POV 
 float correction = .85;
 float offset = 0;
 
-static const int WIFI_POLLING_INTERVAL_MS = 5000;
-
+const char *SERVER_IP = "192.168.0.102";
+const char *SERVER_PORT = "8266"; //port used by the computer
+const int UDP_PORT = 8266;        //port used by Flask
+const int WIFI_POLLING_INTERVAL_MS = 200;
+const int BUFFER_SIZE = 2000; //will allow for a 64x10x3 matrix and some header info
 float last_millis = 0;
+uint8_t buffer[BUFFER_SIZE];
+uint8_t prev_buffer_state = 0;
 
-volatile bool update_flag = false;
+volatile bool update_flag = false; // tells main loop when new sector is reached
 
-#define abs(x) ((x)>0?(x):-(x)) //esp8266 doesn't have abs for floats for some reason, so this works for floats
+// #define abs(x) ((x) > 0 ? (x) : -(x)) //esp8266 doesn't have abs for floats for some reason, so this works for floats
 
-void ICACHE_RAM_ATTR TimerHandler() {
-  if (spin_CW) {
+void ICACHE_RAM_ATTR TimerHandler()
+{
+  if (spin_CW)
+  {
     Driver.increment_sector();
-  } else {
+  }
+  else
+  {
     Driver.decrement_sector();
   }
   Driver.increment_count();
@@ -121,48 +143,58 @@ void readSensorEvent()
   avg_dps = total / NUM_READINGS;
   //  avg_dps = dps;
 
-  if (diff > threshold && prev_diff < threshold) { //on beginning of rotation
+  if (diff > threshold && prev_diff < threshold)
+  { //on beginning of rotation
     prev_diff = diff;
-    if (imu1.getRotationZ() > 0) {
+    if (imu1.getRotationZ() > 0)
+    {
       spin_CW = false;
-    } else {
+    }
+    else
+    {
       spin_CW = true;
     }
     ITimer.enableTimer();
     ITimer.setInterval(50 * 1000, TimerHandler); //timer 0 trigger (calls updateDisplay almost immediately)
-
-  } else if (diff < threshold && prev_diff > threshold) { //module is not rotating
+  }
+  else if (diff < threshold && prev_diff > threshold)
+  { //module is not rotating
     prev_diff = diff;
     ITimer.stopTimer();
   }
-  //  Serial.print(diff);
-  //  Serial.print("\t");
-  //  Serial.print(a1[0]);
-  //  Serial.print("\t");
-  //  Serial.print(a1_c);
-  //  Serial.print("\t");
-  //  Serial.print(a2[0]);
-  //  Serial.print("\t");
-  //  Serial.print(a2_c);
-  //  Serial.print("\t");
-  //  Serial.println(dps);
+  // Serial.print(diff);
+  // Serial.print("\t");
+  // Serial.print(a1[0]);
+  // Serial.print("\t");
+  // Serial.print(a1_c);
+  // Serial.print("\t");
+  // Serial.print(a2[0]);
+  // Serial.print("\t");
+  // Serial.print(a2_c);
+  // Serial.print("\t");
+  // Serial.println(dps);
 }
 
-void ICACHE_RAM_ATTR on_click_ISR() {
-  if (led_state) {
+void ICACHE_RAM_ATTR on_click_ISR()
+{
+  if (led_state)
+  {
     Driver.show_line(10);
     ITimer.stopTimer();
   }
-  else {
+  else
+  {
     Driver.show_line(0);
     ITimer.enableTimer();
   }
   led_state = !led_state;
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   OTA_Setup();
+  udp.begin(UDP_PORT);
 
   Wire.begin();
   imu1.initialize();
@@ -177,62 +209,108 @@ void setup() {
   Driver.show_boot_up_sequence();
 
   // Interval in microsecs
-  ITimer.attachInterruptInterval(500 * 1000, TimerHandler);
-
+  // ITimer.attachInterruptInterval(500 * 1000, TimerHandler);
 }
 
-void loop() {
-  ArduinoOTA.handle();
-  if (update_flag) {
-    Driver.show_sector(false); //params: flip the sector
+void loop()
+{
+  // ArduinoOTA.handle();
+  if (update_flag)
+  {
+    Driver.show_sector(true); //params: flip the sector
+    Driver.print();
     update_flag = false;
   }
-  readSensorEvent();
-  
-  int time_elapsed = millis() - last_millis;
-  if (time_elapsed >= WIFI_POLLING_INTERVAL_MS) {
-    String received_data = "";
-    if (Request_IOT_Data(&received_data, String(avg_dps))) {
-      //      Serial.println(data);
-      correction = received_data.toFloat();
 
-    } else {
-      Serial.println("Failed");
+  // Driver.show_sector(false);
+  // readSensorEvent();
+
+  int time_elapsed = millis() - last_millis;
+  if (time_elapsed >= WIFI_POLLING_INTERVAL_MS)
+  {
+    if (Request_UDP_Data(buffer, String(Driver.get_curr_sector())))
+    {
+      Process_Buffer(buffer);
+    }
+    else
+    {
+      Serial.println("Request to server failed");
     }
     last_millis = millis();
   }
 }
 
-bool Request_IOT_Data(String* inbound, String outbound) {
-  if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
+void Process_Buffer(uint8_t *buffer)
+{
+  switch (buffer[0])
+  {
+  case 0: //update
+    // Serial.println("Update");
+    Driver.set_display_array(
+        &buffer[5], //display array starting at this index
+        buffer[2],  //num_sectors
+        buffer[3],  //num_pixels
+        buffer[4]); //num_colors
+    if (Driver.get_curr_sector() != buffer[1])
+    {
+      update_flag = true;
+    }
+    Driver.set_curr_sector(buffer[1]);
+    break;
+  case 1: //control
+    Serial.println(buffer[1], DEC);
+    Driver.set_curr_sector(buffer[1]);
+    break;
+  default:
+    Serial.println("Ignore");
+    break;
+  }
+  prev_buffer_state = buffer[0];
+}
 
-    HTTPClient http;  //Declare an object of class HTTPClient
+bool Request_UDP_Data(uint8_t *buffer, String outbound)
+{
+  udp.beginPacket(SERVER_IP, UDP_PORT);
+  udp.write(outbound.c_str(), outbound.length());
+  udp.endPacket();
+  memset(buffer, 0, BUFFER_SIZE);
+  //processing incoming packet, must be called before reading the buffer
+  udp.parsePacket();
+  return (udp.read(buffer, BUFFER_SIZE) > 0);
+}
 
-    http.begin("http://192.168.0.102:8266/esp_post"); //Specify request destination
-    http.addHeader("Content-Type", "text/plain");  //Specify content-type header
+bool Request_TCP_Data(String *inbound, String outbound)
+{
+  if (WiFi.status() == WL_CONNECTED)
+  { //Check WiFi connection status
 
-    int httpCode = http.POST(outbound);   //Send the request with data
-    if (httpCode > 0) { //Check the returning code
-      *inbound = http.getString();   //Get the request response payload
-      
+    WiFiClient client;
+    HTTPClient http; //Declare an object of class HTTPClient
+
+    http.begin(client, "http://192.168.0.102:8266/esp_request"); //Specify request destination
+    http.addHeader("Content-Type", "text/plain");                //Specify content-type header
+
+    int httpCode = http.POST(outbound); //Send the request with data
+    if (httpCode > 0)
+    {                              //Check the returning code
+      *inbound = http.getString(); //Get the request response payload
       http.end();
       return true;
-
-    } else {
-      Serial.println("An error ocurred");
     }
-
-    http.end();   //Close connection
+    Serial.println("An error ocurred");
+    http.end(); //Close connection
   }
   return false;
 }
 
 // The boiler plate code to allow for OTA updates
-void OTA_Setup() {
+void OTA_Setup()
+{
   Serial.println("Booting");
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+  WiFi.begin(SSID, PASS);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED)
+  {
     Serial.println("Connection Failed! Rebooting...");
     delay(2000);
     ESP.restart();
@@ -269,11 +347,16 @@ void OTA_Setup() {
   });
   ArduinoOTA.onError([](ota_error_t error) {
     Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    if (error == OTA_AUTH_ERROR)
+      Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR)
+      Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR)
+      Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR)
+      Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR)
+      Serial.println("End Failed");
   });
   ArduinoOTA.begin();
   Serial.println("Ready");
